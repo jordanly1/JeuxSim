33f469513b8ce1ef462505827962606e96a8f108
Ly <2475441@bdeb.qc.ca>
2026-03-16 10:49:12 -0400

    Module4

---
+++ Module04/Assets/Scripts/Controleur.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Controleur : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quitter()
+    {
+#if UNITY_EDITOR
+
+        EditorApplication.isPlaying = false;
+
+#else
+
+  Application.Quit();
+
+#endif
+
+    }
+
+}
