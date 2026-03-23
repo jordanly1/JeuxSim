09360484b9a9bc2e7f628caac02858bc3de85cc3
Jordan Ly <jordanly0109@gmail.com>
2026-03-22 21:56:51 -0400

    Update

--- Modele05/Assets/Scripts/Patrouille.cs
+++ Modele05/Assets/Scripts/Patrouille.cs
@@ -6,17 +6,34 @@ public class Patrouille : MonoBehaviour
     [SerializeField]
     private Transform[] pointsPatrouille;
     private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private int pointMaintenant;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var destionation = pointsPatrouille[Random.Range(0, pointsPatrouille.Length - 1)];
 
-        navMeshAgent.SetDestination(destionation.position);
+        while (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+        {
+            AllerProchainPoint();
+        }
+
+        if (navMeshAgent.velocity.magnitude > 0.1f)
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);
+    }
+
+    void AllerProchainPoint()
+    {
+        pointMaintenant = Random.Range(0, pointsPatrouille.Length);
+        navMeshAgent.SetDestination(pointsPatrouille[pointMaintenant].position);
+        
     }
 }
